ece7199ca5fcecec63d5fc8eb2bd370c39cd9662
git-williamli <3312645820@qq.com>
2020-05-07 17:22:05 +0800

    springbootDuing

---
+++ springbootDuing/src/main/java/com/duing/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.duing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 声明入口类
+ */
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);
+
+
+
+    }
+}
